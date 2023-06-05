9685af909721471c84e7df292db86147d7f69296
Miftah Afina <miftah.afina@gmail.com>
2023-06-05 08:55:33 +0700

    Declarative

---
+++ public/02.declarative.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const output = item => console.log(item);
+
+numbers.forEach(output)
+
+// stay away from:
+// for while do while
+// if else
+// switch
+// instead, use ternary
+
+// break into small function
